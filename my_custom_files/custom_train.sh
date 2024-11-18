8b1d9b3c1b53a3a61b441ed7f8aa90958293c398
Artem Kushneruk <a.kushneruk2003@gmail.com>
2024-11-18 08:48:08 +0300

    change output dir

--- my_custom_files/custom_train.sh
+++ my_custom_files/custom_train.sh
@@ -5,7 +5,7 @@
 # python3 truncate.py 8192 output/truncated-8k
 
 accelerate launch finetune.py \
-    --output-dir output/yarn-7b-64k \
+    --output-dir output/tinydoplhin-1.1b-64k \
     --model cognitivecomputations/TinyDolphin-2.8-1.1b \
     --max-train-steps 30 \
     --dataset emozilla/pg_books-tokenized-bos-eos-chunked-65536
